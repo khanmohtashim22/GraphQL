2a94dbc973c6a44bd59c916847abcd5593771358
Mohtashim Khan <khanmohtashim22@gmail.com>
2021-12-22 13:21:41 -0500

    section complelte on to the next

--- Lyrical-GraphQL/README.md
+++ Lyrical-GraphQL/README.md
@@ -1,2 +1,12 @@
 # Lyrical-GraphQL
 Starter project from a GraphQL course on Udemy.com
+
+
+# mongo db cluster username and password
+username: moe
+password: moeps3
+
+# ports
+client: http://localhost:4000/
+graphical: http://localhost:4000/graphql
+
---
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
+import gql from 'graphql-tag'
+import fetchSongs from '../queries/fetchSongs'
+
+const SongList = ({ data, mutate }) => {
+  const { loading, songs, refetch } = data
+
+  const onSongDelete = id => {
+    mutate({
+      variables: {
+        id
+      }
+    }).then(() => refetch())
+  }
+
+  if (loading) {
+    return <div>...loading</div>
+  }
+
+  return (
+    <div>
+      <ul className='collection'>
+        {
+          songs.map(({ id, title }) => (
+            <li 
+              key={id} 
+              className='collection-item'
+            >
+              {title}
+              <i 
+                className='material-icons' 
+                onClick={() => onSongDelete(id)}
+              >delete</i>
+            </li>
+          ))
+        }
+      </ul>
+      <Link to="/songs/new" className='btn-floating btn-large red right'>
+        <i className='material-icons'>add</i>
+      </Link>
+    </div>
+  )
+}
+
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+      title
+    }
+  }
+` 
+
+// **** NOTE query is not RETURNED instead a data object is returned.
+
+// Also component will rerender twice. Once with no songs and it will make the request
+// then component will rerender again finally with songs on object
+
+// loading is from data object
+export default graphql(mutation)(
+  graphql(fetchSongs)(SongList)
+)
